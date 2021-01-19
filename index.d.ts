495a7b388f93617279fded82f2059f7a81722a54
Ignacio <nacho.pd.87@gmail.com>
2021-01-19 13:58:54 -0300

    Fix type

--- index.d.ts
+++ index.d.ts
@@ -149,6 +149,6 @@ export class MyAlgoWalletWithIframe extends MyAlgoWallet {
 	on(eventName: EventNames, callback: onUpdate): void;
 	off(eventName: EventNames, callback: onUpdate): void;
 	getAccounts(): Promise<StoredAccount[]>;
-	signTx(tx: any, password: string, accountId: string): PromisePromise<SignedTx>;
+	signTx(tx: any, password: string, accountId: string): Promise<SignedTx>;
 	getSettings(): Promise<any>;
 }
